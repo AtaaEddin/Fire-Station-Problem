9e60f11ce16ae4ba79409c879f30216acf4f7270
ataa <antiprosataa@gmail.com>
2019-05-24 22:39:51 +0300

    First commit

---
+++ planning.py
@@ -0,0 +1,16 @@
+""" 
+class Worker 
+	postion : (junior - senior - manager - director)
+	availability : free or busy
+	def handle_call
+
+
+json Call
+	priority : high or low
+	situation : not allocated yet -  allocated - deallocated - push_again
+
+
+def call_generator()
+	
+
+"""
